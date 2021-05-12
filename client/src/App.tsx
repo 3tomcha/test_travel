b8b554ad99804700608835e3b13cf3021e2d1fb9
Tomoya Kobayashi <tomocha.marika@gmail.com>
2021-05-12 14:08:17 +0900

    useState, useEffectを利用する方法に変更

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,43 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+const App = () => {
+  const [ storageValue, setStorageValue ] = useState(0);
+  const [ web3, setWeb3 ] = useState(null);
+  const [ accounts, setAccounts ] = useState(null);
+  const [ contract, setContract ] = useState(null);
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
+  useEffect(() => {
+    const f = async() => {
+      try {
+        // Get network provider and web3 instance.
+        const web3 = await getWeb3();
+  
+        // Use web3 to get the user's accounts.
+        const accounts = await web3.eth.getAccounts();
+  
+        // Get the contract instance.
+        const networkId: string = await web3.eth.net.getId();
+        const networks: object = SimpleStorageContract.networks;
+        const deployedNetwork = (networks as any)[networkId];
+        const instance = new web3.eth.Contract(
+          SimpleStorageContract.abi,
+          deployedNetwork && deployedNetwork.address,
+        );
+  
+        setWeb3(web3);
+        setAccounts(accounts);
+        setContract(instance);
+      } catch (error) {
+        // Catch any errors for any of the above operations.
+        alert(
+          `Failed to load web3, accounts, or contract. Check console for details.`,
+        );
+        console.error(error);
+      }
+    };
+    f();
+  }, []);
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId: string = await web3.eth.net.getId();
-      const networks: object = SimpleStorageContract.networks;
-      const deployedNetwork = (networks as any)[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance });
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
-
-  render() {
-    if (!this.state.web3) {
+    if (!web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
@@ -52,10 +57,9 @@ class App extends Component {
         <p>
           Try changing the value stored on <strong>line 40</strong> of App.js.
         </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div>The stored value is: {storageValue}</div>
       </div>
     );
-  }
 }
 
 export default App;
