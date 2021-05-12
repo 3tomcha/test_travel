47d604125bb6dcc92fd8f14c8230a97253cb90df
Tomoya Kobayashi <tomocha.marika@gmail.com>
2021-05-12 18:46:22 +0900

    display logo

--- client/src/Home.tsx
+++ client/src/Home.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import Header from './components/Header';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <Row>
+                <Col><Header/></Col>
+            </Row>
+            <Row>
+                <Col></Col>
+            </Row>
+            <Row>
+                <Col></Col>
+                <Col></Col>
+            </Row>
+        </div>
     );
 };
 
