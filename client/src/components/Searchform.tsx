1e4bc811f2a986757274bc9db423d29587c2ecf4
Tomoya Kobayashi <tomocha.marika@gmail.com>
2021-05-19 17:01:13 +0900

    フォームで選択した日付をURLに加えることができた

--- client/src/components/Searchform.tsx
+++ client/src/components/Searchform.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, FormEvent } from 'react';
 import { Card, FormControl, FormGroup, InputGroup } from 'react-bootstrap';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker-cssmodules.min.css";
+var applicationID = process.env.applicationID;
+// var url = `https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426?applicationId=${applicationID}&format=json&largeClassCode=japan&middleClassCode=akita&smallClassCode=tazawa&checkinDate=2021-12-01&checkoutDate=2021-12-02&adultNum=2`;
 
 const Searchform = () => {
     const today = new Date();
@@ -9,6 +11,30 @@ const Searchform = () => {
     tomorrow.setDate(tomorrow.getDate() + 1);
     const [startDate, setStartDate] = useState<Date | null>(today);
     const [endDate, setEndDate] = useState<Date | null>(tomorrow);
+
+    const search = (e: FormEvent) => {
+        e.preventDefault();
+        let url = new URL('https://app.rakuten.co.jp/services/api/Travel/VacantHotelSearch/20170426');
+        let searchParams = new URLSearchParams();
+        // 基本的にはapplicationIdは入っていると考える。必要に応じてエラー処理を加える
+        searchParams.append('applicationId', applicationID || '112111');
+        searchParams.append('checkinDate', formatDate(startDate));
+        searchParams.append('checkoutDate', formatDate(endDate));
+        url.search = searchParams.toString();
+        console.log(url.href);
+    };
+
+    const formatDate = (date: Date | null): string => {
+        if (!date) {
+            date = today;
+        }
+        // APIで使う2021-12-01といった形で返す
+        const year: string = date.getFullYear().toString();
+        const month: string = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
     return (
         <div>
             <Card>
@@ -33,6 +59,9 @@ const Searchform = () => {
                     <Card.Body>
                         <DatePicker selected={endDate} onChange={(date: Date | null) => setEndDate(date)}/>
                     </Card.Body>
+                    <button onClick={(e) => search(e)}>
+                        検索する
+                    </button>
                 </form>
             </Card>
         </div>
